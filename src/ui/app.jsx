2e3eec89ee7dfe101dd540754771cdd5c5e57d60
Sergey Mirasov <sergeymirasov@protonmail.com>
2020-08-19 21:01:48 +0300

    feat(ui): add slider component

--- src/ui/app.jsx
+++ src/ui/app.jsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import { Switcher } from './switcher';
+import { RangeSlider } from './range-slider';
 
 export const App = () => {
   const [isChecked, setChecked] = useState(false);
   const toggleSwitcher = () => setChecked(!isChecked);
+
+  const [progress, setProgress] = useState(0);
+  const changeProgress = ({ target }) => setProgress(target.value);
 
   return (
     <StyledApp>
@@ -14,6 +18,17 @@ export const App = () => {
         checked={isChecked}
         onCheck={toggleSwitcher}
         backdropActiveColor="green"
+      />
+
+      <br />
+
+      <RangeSlider
+        min="0"
+        max="100"
+        step="20"
+        value={progress}
+        onChange={changeProgress}
+        width="300px"
       />
     </StyledApp>
   );
---
+++ src/ui/range-slider.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const RangeSlider = ({ min, max, value, onChange, step, width }) => {
+  return (
+    <StyledRangeSlider
+      type="range"
+      min={min}
+      max={max}
+      step={step}
+      value={value}
+      onChange={onChange}
+      width={width}
+    />
+  );
+};
+
+const StyledRangeSlider = styled.input`
+  max-width: ${({ width }) => width};
+  width: 100%;
+  height: 10px;
+  border-radius: 5px;
+  background-color: #ccc;
+  outline: none;
+  opacity: 0.8;
+  transition: opacity 0.2s;
+  appearance: none;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &::-webkit-slider-thumb {
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: tomato;
+    box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.6);
+    cursor: pointer;
+    appearance: none;
+  }
+
+  &::-moz-range-thumb {
+    width: 20px;
+    height: 20px;
+    border: none;
+    border-radius: 50%;
+    background-color: tomato;
+    box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.6);
+    cursor: pointer;
+  }
+
+  &::-moz-range-progress {
+    height: 10px;
+    border-radius: 5px;
+    background-color: tomato;
+  }
+`;
